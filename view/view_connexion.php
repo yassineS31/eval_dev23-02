e0a945f3160e5b70a5a523c40b128f0e1581ddc9
yassineS31 <yassinesebaihia1@gmail.com>
2023-10-25 13:58:42 +0200

    declaration_php

---
+++ view/view_connexion.php
@@ -0,0 +1,16 @@
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>Connexion :</title>
+</head>
+<body>
+<h1>Connexion : </h1>
+<form action="" method="post">
+<p>Veuillez saisir votre identifiant :</p>
+<input type="text" name="login">
+<p>Veuillez saisir votre mot de passe :</p>
+<input type="text" name="mdp">
+<input type="submit" value="connexion">
+</form>
+</body>
+</html>
